7030b1fb3506b84b789ee0614d7eeb8c95f47629
John <jwbarand@gmail.com>
2024-08-09 16:01:54 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(name) {
+    let messages = []
+	for (let i = 0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+		}
+ return messages;
+	
+}
+
+function countDown(number){
+	for (let i = number; i >= 0; i--){
+
+console.log(i)
+	}
+}
